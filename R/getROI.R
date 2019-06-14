86870b75d06a4fff9098c77ee10dcbbd1bb7157b
David Ornelles <ornelles@wakehealth.edu>
2019-06-13 21:14:55 -0400

    still adding

---
+++ R/getROI.R
@@ -0,0 +1,177 @@
+#' Get Region of Interest
+#' 
+#' Get a rectangular region of interest from an image.
+#' 
+#' @param img An \code{Image} object
+#' @param x,y \code{x,y} coordinates of one or both corners of
+#'   rectangular selection \emph{or} the location of the center of the
+#'   rectangular selection \emph{or} a list of length 2 specifying
+#'   the corners of the selection (see details) 
+#' @param x2,y2 optional second pair of x and y coordinates when
+#'   needed to specify the other corner of the rectangular selection 
+#' @param w,h optional width and height of the rectangular selection;
+#'   required if \code{x2,y2} are missing 
+#' @param asCorner \code{logical} value indicating whether to use the
+#'   location of the point \code{x,y} as the corner of the selection identified
+#'   in \code{which.corner} or as the center of the selection (default)
+#' @param which.corner a character vector of length 1 identifying the
+#'   corner of the rectangle specified by \code{x,y};
+#'   applies only if \code{asCorner = TRUE}
+#' @param pch plotting character used by \code{locator}, default of 3 (cross)
+#'   when interacting with the image
+#' @param col color for plotting character used by \code{locator}, default of
+#'   \code{"magenta"}; use \code{NA} for no plotting character
+#' @param border border color of rectangle if \code{markup = TRUE}; if not
+#'   specified, the value for \code{col} is used
+#' @param lwd line width of rectangle if \code{markup = TRUE}
+#' @param markup \code{logical} value indicating if the image should be
+#'   redrawn with the outline drawn on the image
+#' @param asImage \code{logical} value indicated whether the function
+#'   returns the region of interest as an \code{Image} or a list of
+#'   length two containing the \code{x,y} coordinates of the region of interest
+#' 
+#' @seealso addInset; other stuff
+#' 
+#' @details
+#' This allows the interactive or programmatic definition of a rectangular
+#' region of interest in an image by either specifying
+#' coordinates that define the rectangle or by invoking \code{\link{locator}}
+#' to allow the user to interactively select the region of interest.
+#' The region of interest
+#' will be trimmed to the dimensions allowed by the original image.
+#' Options allow specifying the rectangle either by
+#' the center or corner(s) as describe below. The first two options require
+#' no interaction with the user and only produce an image if
+#' \code{markup = TRUE}. The second two options require interaction with the
+#' user. In all cases, the selection coordinates are adjusted to conform to
+#' the dimensions of the image. 
+#' 
+#' \describe{
+#'   \item{List of Length 2}{If \code{x} is a \code{list} of length 2, it
+#'     is assumed to hold opposite corners of the rectangular selection. This
+#'     is the typical result of a call to \code{locator(2)} after having
+#'     plotted the image.}
+#'   \item{Two Points}{If values are provided for each of \code{x,y} and
+#'     \code{x2,y2}, these are treated as opposite corners of
+#'     the rectangular selection.}
+#'   \item{One Point (with width and height)}{If only two values are provided for
+#'     \code{x,y} and \code{x2,y2}, they will be assigned to \code{x,y}. Values
+#'     for \code{w,h} must be provided as named arguments for the width and
+#'     height of the rectangular selection. The point \code{x,y} is
+#'     interpreted as \emph{either} the center (\code{asCorner = FALSE})
+#'     \emph{or} the corner (\code{asCorner = TRUE}) of the selection. If
+#'     \code{asCorner = TRUE}, the position of the corner is determined
+#'     by the argument \code{which.corner} as one of \code{"bottomleft", 
+#'     "topleft", "bottomright",} or \code{"topright"}.}
+#'   \item{No Points (choose opposite corners)}{If all of \code{x,y, x2,y2, w,h}
+#'     are missing, \code{\link{locator}} will allow the user to select two 
+#'     points that define opposite corners of the rectangular selection.}
+#' }
+#'
+#' If \code{asImage = FALSE}, the returned value can be used as to extract the
+#' region of interest from the image or related images by a call such as:
+#' \preformatted{
+#'    pp <- getROI(img, asImage = FALSE)
+#'    pp <- lapply(pp, function(v) seq.int(v[1], v[2]))
+#'    idx <- lapply(dim(img), seq.int)
+#'    idx[1:2] <- pp
+#'    cropped <- do.call("[", c(list(img), idx))
+#' }
+#' 
+#' @return
+#' Either an image an \code{Image} object corresponding to the region of
+#' interest in the image \emph{or} a list of length two containing the
+#' \code{x,y} coordinates of the region of interes.
+#' 
+#' @examples
+#' # Example using fixed width and height to retrieve image
+#'   lighthouse <- readImage(system.file("inst", "extdata", "lighthouse.jpg", package="EBImageExtra"))
+#'   roi <- getROI(lighthouse, 510, 280, w = 200, h = 300)
+#'   plot(roi)
+#'   corners <- getROI(lighthouse, 510, 280, w = 200, h = 300, asCorner = TRUE,
+#'      which.corner = "topleft", asImage = FALSE)
+#'   print(corners)
+#'
+#' @import EBImage
+#' 
+#' @export
+#' 
+getROI <- function(img, x, y, x2, y2, w, h, asCorner = FALSE,
+  which.corner = c("bottomleft", "topleft", "bottomright", "topright"),
+  pch = 3, col = "magenta", border = col, lwd = 2, markup = TRUE,
+  asImage = TRUE)
+{
+  if (!require("EBImage")) stop("This requires the EBImage package")
+
+  if(!is(img, "Image")) stop("'img' must be an Image object")
+  dm <- dim(img)[1:2]
+  which.corner <- sub("upper", "top", which.corner)
+  which.corner <- sub("lower", "bottom", which.corner)
+  which.corner = match.arg(which.corner)
+
+# list of flags for missing arguments
+  F <- c(missing(x), missing(y), missing(x2), missing(y2),
+      missing(w), missing(h))
+
+# process based on the arguments
+  if (all(F[1:6])) { # nothing provided except image
+    plot(img)
+    pp <- locator(2, type = "p", pch = pch, col = col)
+    pp <- lapply(pp, sort)
+  }
+  else if (!any(F[1:4])) {# inset specified, done
+    pp <- list(x = sort(c(x, x2)), y = sort(c(y, y2)))
+  }
+  else if (!F[1] & all(F[2:4])) { # only 'x', must be list of corners
+    if (is(x, "list") && length(x) == 2 && all(lengths(x) == 2))
+      pp <- setNames(lapply(x, sort), c("x", "y"))
+    else
+      stop ("if only 'x' is provided, it must be a list of two points")
+  }
+  else if (!any(F[5:6])) { # 'w' and 'h' provided
+    if (any(F[1:2])) { # need to get one point
+      plot(img)
+      p <- locator(1, type = "p", pch = pch, col = col)
+    }
+    else # 'x' and 'y' provided as the one point
+      p <- list(x = x, y = y)
+  # adjust the one point
+    if (asCorner == TRUE) {
+      if (which.corner == "bottomleft")
+        pp <- list(x = c(p$x, p$x + w), y = c(p$y, p$y - h))
+      else if (which.corner == "topleft")
+        pp <- list(x = c(p$x, p$x + w), y = c(p$y, p$y + h))
+      else if (which.corner == "bottomright")
+        pp <- list(x = c(p$x - w, p$x), y = c(p$y, p$y - h))
+      else # which.corner == "topright"
+        pp <- list(x = c(p$x - w, p$x), y = c(p$y, p$y + h))
+    }
+    else # asCorner == FALSE
+      pp <- list(x = p$x + c(-1, 1)*w/2, y = p$y + c(-1, 1)*h/2)
+  }
+  else # 'w' and 'h' NOT provided
+    stop("need 'w,h' values if only one pair of 'x,y' values is provided")
+
+# adjust the inset coordinates to the limits of the image
+  pp$x <- pmax(1, pmin(pp$x, dm[1]))
+  pp$y <- pmax(1, pmin(pp$y, dm[2]))
+  pp <- lapply(pp, round)
+  pp <- lapply(pp, sort)
+
+# highlight the inset in the image if requested
+  if (markup == TRUE) {
+    plot(img)
+    rect(pp$x[1], pp$y[2], pp$x[2], pp$y[1], border = border, lwd = lwd)
+  }
+
+# return image or adjusted corners of roi
+  if (asImage == TRUE) {
+    pp <- lapply(pp, function(v) seq.int(v[1], v[2]))
+    idx <- lapply(dim(img), seq.int)
+    idx[1:2] <- pp
+    ans <- do.call("[", c(list(img), idx))
+  }
+  else
+    ans <- pp
+  return(ans)
+}
--- R/placeROI.R
+++ R/placeROI.R
@@ -1,28 +1,27 @@
 ##
-## putInset - add a scaled inset to the given image and return marked-up
+## placeROI - place a scaled inset (roi) into an image as an inset
 ##
-putInset <- function(ins, img, position, frac = NULL, mag = NULL,
-	col = "white", frame.width = 2, frac.default = 1/3, ...)
+placeROI <- function(roi, img, position, frac = NULL, mag = NULL,
+	frac.default = 1/3, showImage = TRUE)
 {
-# argument checks
-	if(!is(ins, "Image"))
-		stop ("'ins' must be an Image object")
+# Check arguments
+	if(!is(roi, "Image"))
+		stop ("'roi' must be an Image object")
 	if (is.character(img)) {
-		if (file.exists(f[[1]])
+		if (file.exists(img[1]))
 			img <- readImage(img)
 		else
 			stop("'img' is not a valid image filename")
+	}
 	else if (!is(img, "Image"))
 		stop("'img' must be an Image object or valid filename")
 
+# Harmonize colorMode
 	cmx <- colorMode(img)
-	cm2 <- colorMode(ins)
-	if (cm1 != cm2)
-		warning("grayscale image converted to RGB")
-	if (cmx > cm2)
-		ins <- toRGB(ins)
-	else if (cm2 > cmx)
-		img <- toRGB(img)
+	cm2 <- colorMode(roi)
+	if (cmx != cm2) warning("grayscale image promoted to RGB")
+	if (cmx > cm2) roi <- toRGB(roi)
+	else if (cm2 > cmx) img <- toRGB(img)
 
 # Common variables
 	choices <- c("topleft", "top", "topright", "left", "center", "right",
@@ -30,16 +29,16 @@ putInset <- function(ins, img, position, frac = NULL, mag = NULL,
 	choices <- factor(choices, levels = choices)
 
 	dm.img <- dim(img)[1:2]
-	dm.ins <- dim(ins)[1:2]
+	dm.roi <- dim(roi)[1:2]
 	cmx <- colorMode(img)
 
 # Determine magnification 'mag' from fractional size 'frac'
 	if (is.null(frac) & is.null(mag)) { # given neither 'mag' nor 'frac'
 		frac <- frac.default
-		mag <- frac * dm.img[1]/dm.ins[1]
+		mag <- frac * dm.img[1]/dm.roi[1]
 	}
 	else if (is.null(frac) & !is.null(mag)) { # given 'mag'
-		frac <- mag * dm.ins / dm.img
+		frac <- mag * dm.roi / dm.img
 		if (any(frac[1:2] >= 1))
 			stop("inset will exceed size of image with mag = ", signif(mag, 2))
 		frac <- frac[1]
@@ -48,33 +47,38 @@ putInset <- function(ins, img, position, frac = NULL, mag = NULL,
 		if (frac > 1 | frac < 0)
 			stop("'frac' must be between 0 and 1")
 		frac <- frac
-		mag <- frac * dm.img[1]/dm.ins[1]
+		mag <- frac * dm.img[1]/dm.roi[1]
 	}
 	else if (!is.null(frac) & !is.null(mag)) { # given both
-		if (signif(mag, 3) != signif(frac * dm.img[1]/dm.ins[1]))
+		if (signif(mag, 3) != signif(frac * dm.img[1]/dm.roi[1]))
 			stop("incompatible 'mag' and 'frac' values provided, use one or the other")
 		else
 			mag <- mag
 	}
 
-# Resize inset by mag and recalculate dm.ins
-  ins <- resize(ins, w = mag * dm.ins[1])
-  dm.ins <- dim(ins)[1:2]
+# Resize inset by mag and recalculate dm.roi
+  roi <- resize(roi, w = mag * dm.roi[1])
+  dm.roi <- dim(roi)[1:2]
 
 # Determine position for inset from 'position' and 'choices'
 	if (!missing(position)) {
-			if (is(position, "character"))
+			if (is(position, "character")) {
+				position <- sub("upper", "top", position)
+				position <- sub("lower", "bottom", position)
 				position <- match.arg(position, choices)
+			}
 			else if (is.numeric(position) && position > 0 && position <= length(choices))
 				position <- levels(choices)[position]
 			else
-				stop("'position' must be a character or integer in [1,9]")
+				stop("'position' must be a character or integer in 1 through 9")
 	}
 	else {
 		nx <- 3 # three "zones" across and down
 		vx <- seq(0, dm.img[1] + 1, length = nx + 1)
 		vy <- seq(0, dm.img[2] + 1, length = nx + 1)
 		i <- 0
+		cat("Click near the corner or side to have inset\n")
+		flush.console()
 		while (!i %in% seq_len(nx*nx)) {
 			p <- locator(1)
 			ix <- findInterval(p$x, vx)
@@ -85,35 +89,18 @@ putInset <- function(ins, img, position, frac = NULL, mag = NULL,
 	}
 
 # Calculate translation adjustment for given position
-	pad <- dm.img - dm.ins
-	offset <- switch(position,
+	pad <- dm.img - dm.roi
+	offset <- switch(as.character(position),
 		"topleft" = c(0, 0), "top" = c(0.5, 0), "topright" = c(1, 0),
 		"left" = c(0, 0.5), "center" = c(0.5, 0.5), "right" = c(1, 0.5),
 		"bottomleft" = c(0, 1), "bottom" = c(0.5, 1), "bottomright" = c(1, 1))
 	
-	mask <- Image("black", dim = dm.ins, colormode = 2)
+# Assemble image by creating masks and using array math
+	mask <- Image("black", dim = dm.roi, colormode = 2)
 	mask <- translate(mask, offset * pad, output.dim = dm.img, bg.col = "white")
-	ins <- translate(ins, offset * pad, output.dim = dm.img, bg.col = "black")
-	ans <- mask * img + ins
-
-# Add possible frame to inset
-# Method 1 - replace pixels in situ ## Not quite, how to specify color across matrix
-#	inc <- ceiling(lwd)
-#	p1 <- lapply(pp, function(v) c(seq(v[1], len = inc), seq(v[2] - inc + 1, len = inc)))
-#	p2 <- lapply(pp, function(v) seq(v[1], v[2]))
-#	img[p1$x, p2$y, ] <- 1
-#	img[p2$x, p1$y, ] <- 1
-	
-# Method 2 - paint onto image, save and reload ## NOT CORRECT
-	rect(p$x[1], p$y[1], p$x[2], p$y[2], border = col, lend = lend, lwd = lwd, ...)
-	rect(pp$x[1], pp$y[1], pp$x[2], pp$y[2], border = col, lend = lend, lwd = lwd/frac, ...)
-
-# Write and retrieve the new image
-	ftemp <- tempfile()
-	dev.print(tiff, ftemp, width = dm.img[1], height = dm.img[2], bg = "transparent")
-	dev.off()
-	img <- readImage(ftemp, type = "tiff")
-	file.remove(ftemp)
-	plot(img)
-	invisible(img)
+	roi <- translate(roi, offset * pad, output.dim = dm.img, bg.col = "black")
+	ans <- mask * img + roi
+	if (showImage == TRUE)
+		plot(ans)
+	return(ans)
 }
