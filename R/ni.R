1671924497607233ac2c03e2e7abb4416f0580b3
David Ornelles <ornelles@wakehealth.edu>
2019-06-15 23:42:05 -0400

    use single digit as bit-depth specification

--- R/ni.R
+++ R/ni.R
@@ -4,9 +4,10 @@
 #' \code{inputRange} appropriate for 12-bit images.
 #' 
 #' @param x An \code{Image} object or array or a list of \code{Image} objects
-#' @param ir A numeric vector or a list of vectors to set the input
-#'   intensity range. This value is passed to \code{\link[EBImage]{normalize}}
-#'   as the \code{inputRange} parameter
+#' @param ir A numeric vector or a list of vectors or a single numeric
+#'   value indicating the bit depth
+#'   to set the input intensity range. This value is passed to
+#'   \code{\link[EBImage]{normalize}} as the \code{inputRange} parameter
 #' @param separate If \code{TRUE} or if \code{ir} is \code{NULL}, each frame
 #'   will be normalized independently
 #' @param ft A numeric vector of 2 values or \code{list} of vectors to be the
@@ -18,10 +19,15 @@
 #' 12-bit images. This function provides an easier-to-type replacement for 
 #' \code{\link{normalize}} while extending it to accept lists of images.
 #' 
+#' If \code{ir} is a \code{numeric} value of length 1 that is greater than
+#' 1 and less than or equal to 16, it is interpreted as the
+#' bit depth of the image and the value for \code{ir} is given by 
+#' \code{c(0, 2^-(16-ir)}. This allows the use of values such as \code{11.5}
+#' to increase the brightness of a 12-bit image.
+#' 
 #' @return
 #' An \code{Image} object or array or a \code{list} of such objects containing
 #' the transformed object(s).
-#' 
 #'
 #' @import EBImage
 #' 
@@ -36,8 +42,11 @@ ni <- function(x, ir = c(0, 2^-4), separate = TRUE, ft = c(0, 1))
 	.fun <- function(x, ir, separate, ft) {
 		if (is.null(x))
 			x
-		else
-			normalize(x, separate = separate, ft = ft, inputRange = ir)
+		else {
+			if (length(ir) == 1 && ir > 1 && ir <= 16)
+				ir <- c(0, 2^(ir - 16))
+			EBImage::normalize(x, separate = separate, ft = ft, inputRange = ir)
+		}
 	}
 # process single image
 	if (is(x, "Image"))
