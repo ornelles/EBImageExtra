61fd6d04021789850f5da1e90c33671d843ddeee
David Ornelles <ornelles@wakehealth.edu>
2020-02-09 23:07:17 -0500

    new Roxygen

--- R/ni.R
+++ R/ni.R
@@ -8,22 +8,24 @@
 #'   value indicating the bit depth
 #'   to set the input intensity range. This value is passed to
 #'   \code{\link[EBImage]{normalize}} as the \code{inputRange} parameter
-#' @param separate If \code{TRUE} or if \code{ir} is \code{NULL}, each frame
-#'   will be normalized independently
+#' @param separate If \code{TRUE} each frame will be normalized independently
+#'   and \code{ir} will be ignored
 #' @param ft A numeric vector of 2 values or \code{list} of vectors to be the
 #'   target minimum and maximum intensity values after normalization
 #' 
 #' @details
-#' See \code{\link[EBImage]{normalize}} for details. The default here is to use
-#' an \code{inputRange} of \code{c(0,2^-4)}, which is appropriate for
+#' See \code{\link[EBImage]{normalize}} for details. The default here differs
+#' from \code{\link{normalize}} the default value for \code{ir} forces
+#' the call to use \code{separate = FALSE}. The default value sets
+#' \code{inputRange} to \code{c(0,2^-4)}, which is appropriate for
 #' 12-bit images. This function provides an easier-to-type replacement for 
 #' \code{\link{normalize}} while extending it to accept lists of images.
 #' 
-#' If \code{ir} is a \code{numeric} value of length 1 that is greater than
-#' 1 and less than or equal to 16, it is interpreted as the
-#' bit depth of the image and the value for \code{ir} is given by 
-#' \code{c(0, 2^-(16-ir)}. This allows the use of values such as \code{11.5}
-#' to increase the brightness of a 12-bit image.
+#' If \code{ir} is a \code{numeric} value of length 1 between 1 and 16,
+#' it is interpreted as the effective bit depth of the input image and
+#' the value for \code{ir} is given by \code{c(0, 2^-(16-ir)}. This allows
+#' the use of values such as \code{11.5} to increase the brightness of a
+#' 12-bit image.
 #' 
 #' @return
 #' An \code{Image} object or array or a \code{list} of such objects containing
@@ -36,17 +38,17 @@
 #'
 #' @export
 #'  
-ni <- function(x, ir = c(0, 2^-4), separate = TRUE, ft = c(0, 1))
+ni <- function(x, ir = c(0, 2^-4), separate = FALSE, ft = c(0, 1))
 {
 # working function
 	.fun <- function(x, ir, separate, ft) {
-		if (is.null(x))
-			x
-		else {
-			if (length(ir) == 1 && ir > 1 && ir <= 16)
-				ir <- c(0, 2^(ir - 16))
-			EBImage::normalize(x, separate = separate, ft = ft, inputRange = ir)
-		}
+		if (is.null(x)) return(NULL)
+		if (length(ir) == 1 && ir > 1 && ir <= 16)
+			ir <- c(0, 2^(ir - 16))
+		if (identical(separate, TRUE))
+			EBImage::normalize(x, separate = separate, ft = ft)
+		else
+			EBImage::normalize(x, inputRange = ir, ft = ft)
 	}
 # process single image
 	if (is(x, "Image"))
